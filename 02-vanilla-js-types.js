ed0b5f89e319d282aa4864b0240d7bb0f9a08d4f
Estela Jalac <jalacestelamae@gmail.com>
2026-03-27 14:56:45 +0800

    Done playing and updating the 02-vanilla-js-types.js file

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,13 @@
-let userName1 = "Elmer";
+let userName1 = "Estela";
 
 console.log(typeof userName1)
+
+let age = 22
+let isActive = true
+let score = 9.5
+let nothing = null
+
+console.log(typeof age)
+console.log(typeof isActive)
+console.log(typeof score)
+console.log(typeof nothing)
